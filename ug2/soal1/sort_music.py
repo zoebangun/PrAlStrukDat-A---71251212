69ea542ecc2ab98e4b1b728a45dbede12a558fca
zoebangun <zoeeloket@gmail.com>
2026-09-17 14:59:11 +0700

    71251212

--- ug2/soal1/sort_music.py
+++ ug2/soal1/sort_music.py
@@ -9,11 +9,28 @@ songs = [
 
 def sort_by_views(songs):
     res = songs.copy()
-    pass
+
+    for i in range(len(res)):
+        for j in range(len(res) - 1 - i):
+            if res[j]["views"] < res[j + 1]["views"]:
+                res[j], res[j + 1] = res[j + 1], res[j]
+
+    return res
+
 
 def sort_by_favourite_genre(songs, favourite_genre):
     res = songs.copy()
-    pass
+
+    for i in range(len(res)):
+        for j in range(len(res) - 1 - i):
+            if (
+                res[j]["genre"] != favourite_genre
+                and res[j + 1]["genre"] == favourite_genre
+            ):
+                res[j], res[j + 1] = res[j + 1], res[j]
+
+    return res
+
 
 print("=== SORT BY VIEWS ===")
 
--- ug2/soal2/sortMahasiswa.py
+++ ug2/soal2/sortMahasiswa.py
@@ -7,20 +7,30 @@ presensi_dummy(data)
 acak_data(data)
 
 
-
-def sort_by(data: list=data, index: str="nim",rev = False):
+def sort_by(data: list = data, index: str = "nim", rev=False):
     maps = {
-        "nim":0,
-        "nama":1,
-        "presensi":2,
+        "nim": 0,
+        "nama": 1,
+        "presensi": 2,
     }
-    
-    # Kerjakan disini
-    
+
+    kolom = maps[index]
+
+    for i in range(len(data)):
+        for j in range(len(data) - 1 - i):
+
+            # Ascending
+            if rev == False:
+                if data[j][kolom] > data[j + 1][kolom]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+
+            # Descending
+            else:
+                if data[j][kolom] < data[j + 1][kolom]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+
     # Jangan Dihapus
     show_data(data)
 
+
 sort_by(data)
-
-
-    
